e5930083194fada51dd764f376241034795635ce
Konstantinos Pittas <konstantinos.pittas@gmail.com>
2016-08-21 00:45:13 +0300

    Fixed tests.

--- tests/IVCalculatorTest.php
+++ tests/IVCalculatorTest.php
@@ -1,16 +1,13 @@
 <?php
 
 use IVCalculator\IVCalculator;
-use IVCalculator\Pokedex;
 
 class IVCalculatorTest extends PHPUnit_Framework_TestCase
 {
     /** @test */
     public function it_gets_possible_ivs()
     {
-        $pokemon = (new Pokedex())->getByName('Zubat');
-
-        $potentialIVs = (new IVCalculator())->evaluate($pokemon, 342, 52, 3000);
+        $potentialIVs = (new IVCalculator())->evaluate('Zubat', 342, 52, 3000);
 
         $this->assertCount(27, $potentialIVs);
     }
